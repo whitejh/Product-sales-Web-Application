10c2324d2394e4756b2e0997df7747fa25fc9461
whitejh <kei02012@naver.com>
2021-03-09 18:28:24 +0900

    watchFile

--- products/server/app.js
+++ products/server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const session = require('express-session');
+const fs = require('fs');
 
 app.use(
   session({
@@ -17,6 +18,14 @@ app.use(
 const server = app.listen(3000, () => {
   console.log('Server started. port 3000.');
 });
+
+let sql = require('./sql.js');
+
+fs.watchFile(__dirname + '/sql.js', (curr, prev) => {
+  console.log('sql 변경시 재시작 없이 반영되도록 함.');
+  delete require.cache[require.resolve('./sql.js')]; // fs 모듈을 통해서 sql.js 파일을 watchFile로 감시하고, 변경이 일어나면 기존에 캐시에 올라가 있는 sql.js를 삭제하고 변경된 파일을 다시 담아줌
+  sql = require('./sql.js');
+}); // 실무에서 많이 쓰는 코드
 
 const db = {
   database: 'dev_class',
@@ -38,14 +47,12 @@ app.post('api/logout', async (request, res) => {
   res.send('ok');
 });
 
-const sql = require('./sql.js');
-
 // 클라이언트에서 서버로 데이터를 넣거나 받기위한 요청이 들어올때, 실제로 아까 만든 sql.js에 이 쿼리를 어떻게 불러와서 데이터를 받아오는지 구현해본다
 
 app.post('/api/:alias', async (request, res) => {
-  if (!request.session.email) {
-    return res.status(401).send({ error: 'You need to login.' });
-  }
+  // if (!request.session.email) {
+  //   return res.status(401).send({ error: 'You need to login.' });
+  // }
 
   try {
     res.send(await req.db(request.params.alias));
--- products/server/sql.js
+++ products/server/sql.js
@@ -1,6 +1,16 @@
 module.exports = {
   productList: {
-    query: `select * from t_product`,
+    query: `SELECT t1.*, t2.path, t3.category1, t3.category2, t3.category3 
+    FROM t_product t1, t_image t2, t_category t3
+    WHERE t1.id = t2.product_id AND t2.type = 1 AND t1.category_id = t3.id`,
+  },
+  productDetail: {
+    query: `SELECT t1.*, t2.path, t3.category1, t3.category2, t3.category3 
+    FROM t_product t1, t_image t2, t_category t3
+    WHERE t1.id = 1 AND t1.id = t2.product_id AND t2.type=3 AND t1.category_id=t3.id`,
+  },
+  productMainImages: {
+    query: `SELECT * FROM t_image WHERE product_id = 1 AND TYPE = 2`,
   },
   sellerList: {
     query: `select * from t_seller`,
