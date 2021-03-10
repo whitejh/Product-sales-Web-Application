745f780dbf6ddde8d6b027e6f2408cb7dbde39d3
whitejh <kei02012@naver.com>
2021-03-10 15:00:34 +0900

    제품리스트 Vue 컴포넌트 구현

--- products/client/src/main.js
+++ products/client/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import mixins from './mixins';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App)
   .use(router)
+  .mixin(mixins)
   .mount('#app');
