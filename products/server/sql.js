dd21d3b279e26437ecaa8c75da6cc44f1992c183
whitejh <kei02012@naver.com>
2021-03-16 11:54:57 +0900

    제품등록 Vue 컴포넌트 구현

--- products/server/sql.js
+++ products/server/sql.js
@@ -1,22 +1,46 @@
 module.exports = {
   productList: {
-    query: `SELECT t1.*, t2.path, t3.category1, t3.category2, t3.category3 
-    FROM t_product t1, t_image t2, t_category t3
-    WHERE t1.id = t2.product_id AND t2.type = 1 AND t1.category_id = t3.id`,
+    query: `select t1.*, t2.path, t3.category1, t3.category2, t3.category3 
+    from t_product t1, t_image t2, t_category t3
+    where t1.id = t2.product_id and t2.type = 1 and t1.category_id = t3.id`,
+  },
+  productList2: {
+    query: `select t3.*, t4.path from (select t1.*, t2.category1, t2.category2, t2.category3 
+      from t_product t1, t_category t2
+      where t1.category_id = t2.id) t3
+      left join (select * from t_image where type=1) t4
+      on t3.id = t4.product_id`,
   },
   productDetail: {
-    query: `SELECT t1.*, t2.path, t3.category1, t3.category2, t3.category3 
-    FROM t_product t1, t_image t2, t_category t3
-    WHERE t1.id = ? AND t1.id = t2.product_id AND t2.type=3 AND t1.category_id=t3.id`,
+    query: `select t1.*, t2.path, t3.category1, t3.category2, t3.category3 
+    from t_product t1, t_image t2, t_category t3
+    where t1.id = ? and t1.id = t2.product_id and t2.type = 3 and t1.category_id = t3.id`,
   },
   productMainImages: {
-    query: `SELECT * FROM t_image WHERE product_id = ? AND TYPE = 2`,
+    query: `select * from t_image where product_id = ? and type = 2`,
   },
   productInsert: {
-    query: `INSERT INTO t_product (product_name, product_price, delivery_price, add_delivery_price, tags, outbound_days, seller_id, category_id)
-    VALUES(?,?,?,?,?,?,?,?);`,
+    query: `insert into t_product set ?`,
+  },
+  productImageInsert: {
+    query: `insert into t_image set ?`,
+  },
+  imageList: {
+    query: `select * from t_image where product_id=?`,
+  },
+  imageDelete: {
+    query: `delete from t_image where id=?`,
+  },
+  productDelete: {
+    query: `delete from t_product where id=?`,
+  },
+  categoryList: {
+    query: `select * from t_category`,
   },
   sellerList: {
     query: `select * from t_seller`,
+  },
+  signUp: {
+    query: `insert into t_user set ? on duplicate key update ?`, //없으면 insert 시키고, 있으면 update 시키는 것
   },
 };
